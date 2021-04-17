27c76b4fed86b677a24d0614c54c3182607855cd
TechXero *~* DarkXero <81101439+TechXero@users.noreply.github.com>
2021-04-18 00:42:38 +0300

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -33,6 +33,8 @@ passwd
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+pacman -S NetworkManager bluetooth
+
 install packages.sh
 
 # Enable services
